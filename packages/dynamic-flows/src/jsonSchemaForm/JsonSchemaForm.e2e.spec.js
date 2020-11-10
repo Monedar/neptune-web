24ae0263ce20a80ac67e6ceb1cfec466550f43e0
Helen Durrant <61203573+helendurranttw@users.noreply.github.com>
2020-11-10 11:22:24 +0000

    fix!: date input onChange updates (#658)
    BREAKING CHANGE: DateInput - onChange when month is not selected (#614)
    
    Before: When month is not selected, it defaults to 0 which is January. This results in the component dispatching `onChange` with a seemingly valid data, when in reality the user hasn't finished composing the date.
    After: When month is not selected, we don't construct a Date object, and the `onChange` event contains `null` instead.
    
    Before: onChange was not being called if the user de-selected a month
    After: onChange is called with null
    
    BREAKING CHANGE: date input onFocus/onBlur grouped events (#626)
    
    DateInput now only triggers “onFocus” and “onBlur” when the focus changes outside of its 3 sub-components, and not when cycling through them.

--- packages/dynamic-flows/src/jsonSchemaForm/JsonSchemaForm.e2e.spec.js
+++ packages/dynamic-flows/src/jsonSchemaForm/JsonSchemaForm.e2e.spec.js
@@ -148,8 +148,16 @@ describe('E2E: Given a component for rendering a JSON schema form', () => {
       component.find('input[type="text"]').simulate('change', { target: { value: 'x' } });
     });
 
-    it('should remove the value from the model', () => {
-      expect(onChange).toHaveBeenCalledWith({ const: 'abcd' }, true, stringSchema);
+    it('should NOT remove the value from the model', () => {
+      expect(onChange).toHaveBeenLastCalledWith(
+        { const: 'abcd', string: 'x' },
+        false,
+        stringSchema,
+      );
+    });
+
+    it('should call onChange with valid = false', () => {
+      expect(onChange.mock.calls[onChange.mock.calls.length - 1][1]).toBe(false);
     });
 
     describe('and then to something else invalid', () => {
@@ -157,8 +165,8 @@ describe('E2E: Given a component for rendering a JSON schema form', () => {
         component.find('input[type="text"]').simulate('change', { target: { value: 'y' } });
       });
 
-      it('should not call onChange again', () => {
-        expect(onChange).toHaveBeenCalledTimes(2);
+      it('should call onChange again', () => {
+        expect(onChange).toHaveBeenCalledTimes(3);
       });
     });
 

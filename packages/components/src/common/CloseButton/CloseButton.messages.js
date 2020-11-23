de94c147c5b976419ac2f9a27ccadfcba71d8ad5
Anton Dozortsev <dozortsevanton@gmail.com>
2020-11-23 22:19:33 +0000

    chore: delete translation files

--- packages/components/src/common/CloseButton/CloseButton.messages.js
+++ packages/components/src/common/CloseButton/CloseButton.messages.js
@@ -1,10 +1,7 @@
 import { defineMessages } from 'react-intl';
 
-const id = 'np.CloseButton';
-
 export default defineMessages({
   ariaLabel: {
-    id: `${id}.aria.label`,
     defaultMessage: 'Close',
   },
 });

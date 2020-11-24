103932a4286e196c8dea60d6ae1d76cf6ed7d601
Anton Dozortsev <dozortsevanton@gmail.com>
2020-11-24 17:35:39 +0000

    chore: a few more messages

---
+++ packages/components/src/lab/pagination/paginationLink/PaginationLink.messages.js
@@ -0,0 +1,10 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  ariaLabelActive: {
+    defaultMessage: 'Go to page {pageNumber}',
+  },
+  ariaLabelPassive: {
+    defaultMessage: 'Current page, page {pageNumber}',
+  },
+});
